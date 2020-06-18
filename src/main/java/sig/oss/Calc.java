bca2faf8c447d2cd99348f9b8e9a2c36ea099f29
kinvyrlfl7 <67099700+kinvyrlfl7@users.noreply.github.com>
2020-06-18 18:52:37 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,8 +1,12 @@
 public class Calc {
+	public int r = 0;
 	public int add(int x, int y) { //add method
 		return x+y;
 	}
 	public int sub(int x, int y) {
 		return x-y;
+	}
+	public int dec(int d) {
+		r -= d;
 	}
 }
